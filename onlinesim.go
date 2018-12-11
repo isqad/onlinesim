64971881606846dfa676e2b4752528fd11af5fb2
Andrew N. Shalaev <isqad88@gmail.com>
2018-12-11 12:21:17 +0500

    working version

--- onlinesim.go
+++ onlinesim.go
@@ -3,6 +3,7 @@ package onlinesim
 import (
 	"crypto/tls"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -16,29 +17,96 @@ import (
 const MaxRetries = 3
 const OnlinesimApiEndpoint = `https://onlinesim.ru/api`
 
-func init() {
-	rand.Seed(time.Now().UnixNano())
+type JsonResponse int
+
+// Разновидности ответов
+const (
+	ResponseSuccess       JsonResponse = iota
+	ResponseFail                       // Ошибка
+	ResponseSmsWait                    // Ожидается ответ
+	ResponseNoNums                     // Нет подходящих номеров
+	ResponseTzInpool                   // Операция ожидает выделения номера
+	ResponseTzOverEmpty                // Ответ не поступил за отведенное время
+	ResponseTzNumAnswer                // Поступил ответ
+	ResponseTzOverOk                   // Операция завершена
+	ResponseErrorNoTzid                // Не указан tzid
+	ResponseErrorNoOps                 // Нет операций
+	ResponseAccountIdFail              // Необходимо пройти идентификацию для заказа номера
+)
+
+func (a *JsonResponse) UnmarshalJSON(b []byte) error {
+	if b[0] == '1' {
+		*a = ResponseSuccess
+		return nil
+	}
+
+	var s string
+	if err := json.Unmarshal(b, &s); err != nil {
+		return err
+	}
+
+	switch s {
+	case "1":
+		*a = ResponseSuccess
+	case "TZ_NUM_WAIT":
+		*a = ResponseSmsWait
+	case "WARNING_NO_NUMS":
+		*a = ResponseNoNums
+	case "TZ_INPOOL":
+		*a = ResponseTzInpool
+	case "TZ_OVER_EMPTY":
+		*a = ResponseTzOverEmpty
+	case "TZ_NUM_ANSWER":
+		*a = ResponseTzNumAnswer
+	case "TZ_OVER_OK":
+		*a = ResponseTzOverOk
+	case "ERROR_NO_TZID":
+		*a = ResponseErrorNoTzid
+	case "ERROR_NO_OPERATIONS":
+		*a = ResponseErrorNoOps
+	case "ACCOUNT_IDENTIFICATION_REQUIRED":
+		*a = ResponseAccountIdFail
+	default:
+		*a = ResponseFail
+	}
+
+	return nil
+}
+
+type NumberResponse struct {
+	Response JsonResponse
+	Tzid     int32
+}
+
+type Msg struct {
+	Service string
+	Msg     string
+}
+
+type StateResponse struct {
+	Response      JsonResponse
+	Tzid          int32
+	Service       string
+	Number        string
+	Msg           []Msg
+	Time          int32
+	Form          string
+	ForwardStatus string
+	ForwardNumber string
+	Country       int32
 }
 
 type BalanceResponse struct {
-	Response string
+	Response JsonResponse
 	Balance  string
 	Zbalance int32
 }
 
-func apiUrl(action string) string {
-	apiKey := os.Getenv("ONLINESIM_API_KEY")
-	return fmt.Sprintf(`%s/%s.php?apikey=%s`, OnlinesimApiEndpoint, action, apiKey)
+func init() {
+	rand.Seed(time.Now().UnixNano())
 }
 
-func client() *http.Client {
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-
-	return &http.Client{Transport: tr}
-}
-
+// Public: получение текущего баланса
 func GetBalance(balance *float64) error {
 	return retry(MaxRetries, time.Second, func() error {
 		var onlineSimResponse BalanceResponse
@@ -46,8 +114,8 @@ func GetBalance(balance *float64) error {
 		resp, err := client().Get(apiUrl(`getBalance`))
 
 		if err != nil {
-			log.Fatal(err)
-			return err
+			log.Print(err)
+			return errors.New("Request failed")
 		}
 
 		defer resp.Body.Close()
@@ -57,21 +125,21 @@ func GetBalance(balance *float64) error {
 		log.Print(string(body[:]))
 
 		if err = json.Unmarshal(body, &onlineSimResponse); err != nil {
-			log.Fatal(err)
-			return nil
-		}
+			log.Print(err)
 
-		if onlineSimResponse.Response != `1` {
-			log.Fatal(onlineSimResponse)
-			return nil
+			return stop{error: err}
 		}
 
 		log.Print(onlineSimResponse)
 
+		if onlineSimResponse.Response == ResponseFail {
+			return stop{error: errors.New("Response Fail")}
+		}
+
 		money, err := strconv.ParseFloat(onlineSimResponse.Balance, 64)
 		if err != nil {
-			log.Fatal(err)
-			return nil
+			log.Print(err)
+			return stop{error: err}
 		}
 
 		*balance = money
@@ -80,99 +148,65 @@ func GetBalance(balance *float64) error {
 	})
 }
 
-type GetNumberResponse struct {
-	Response int32
-	Tzid     int32
+func GetNumber(service string) ([]string, int32, error) {
+	var idOp int32
+
+	states := make([]StateResponse, 2, 2)
+	numbers := make([]string, 2, 2)
+
+	// запросили номер
+	// получили ид операции
+	if err := numberReq(service, &idOp); err != nil {
+		return []string{}, 0, err
+	}
+
+	if err := getState(idOp, 1, &states); err != nil {
+		return []string{}, 0, err
+	}
+
+	for i, s := range states {
+		numbers[i] = s.Number
+	}
+
+	return numbers, idOp, nil
 }
 
-func GetNumber(service string, id *int32) error {
-	return retry(MaxRetries, time.Second, func() error {
-		var getNumResponse GetNumberResponse
+func GetSms(idOp int32, smsText *[]string, tries int) error {
+	return retry(tries, time.Second, func() error {
+		states := make([]StateResponse, 2, 2)
+		texts := make([]string, 2, 2)
 
-		url := fmt.Sprintf(`%s&service=%s&country=86`, apiUrl(`getNum`), service)
-		log.Print(url)
-		resp, err := client().Get(url)
+		getState(idOp, 1, &states)
+		log.Print(states)
+		log.Print(states)
 
-		if err != nil {
-			log.Fatal(err)
-			return err
+		for _, s := range states {
+			if s.Response == ResponseTzNumAnswer {
+				for j, m := range s.Msg {
+					texts[j] = m.Msg
+				}
+			}
+
+			if s.Response == ResponseSmsWait {
+				return errors.New("Waiting response. Retry...")
+			}
+
+			if s.Response != ResponseTzNumAnswer {
+				log.Print(s)
+				return stop{error: errors.New("No valid response")}
+			}
 		}
 
-		defer resp.Body.Close()
-
-		body, _ := ioutil.ReadAll(resp.Body)
-
-		log.Print(string(body[:]))
-
-		if err = json.Unmarshal(body, &getNumResponse); err != nil {
-			log.Fatal(err)
-			return nil
-		}
-
-		if getNumResponse.Response != 1 {
-			log.Fatal(getNumResponse)
-			return nil
-		}
-
-		log.Print(getNumResponse)
-
-		*id = getNumResponse.Tzid
+		*smsText = texts
 
 		return nil
 	})
 }
 
-type StateResponse struct {
-	Response      string
-	Tzid          string
-	Service       string
-	Number        string
-	Msg           string
-	Time          string
-	Form          string
-	ForwardStatus string
-	ForwardNumber string
-	Country       string
-}
+// Private
 
-func GetState(id int32, messageToCode int32, state *StateResponse) error {
-	return retry(MaxRetries, time.Second, func() error {
-		var stateResponse StateResponse
-		url := fmt.Sprintf(`%s&tzid=%d&message_to_code=%d`,
-			apiUrl(`getState`), id, messageToCode)
-
-		log.Print(url)
-
-		resp, err := client().Get(url)
-
-		if err != nil {
-			log.Fatal(err)
-			return err
-		}
-
-		defer resp.Body.Close()
-
-		body, _ := ioutil.ReadAll(resp.Body)
-
-		log.Print(string(body[:]))
-
-		if err = json.Unmarshal(body, &stateResponse); err != nil {
-			log.Fatal(err)
-			return nil
-		}
-
-		if stateResponse.Response != `1` {
-			log.Fatal(stateResponse)
-			return nil
-		}
-
-		log.Print(stateResponse)
-
-		*state = stateResponse
-
-		return nil
-	})
-
+type stop struct {
+	error
 }
 
 // https://upgear.io/blog/simple-golang-retry-function/
@@ -197,6 +231,90 @@ func retry(attempts int, sleep time.Duration, f func() error) error {
 	return nil
 }
 
-type stop struct {
-	error
+func apiUrl(action string) string {
+	apiKey := os.Getenv("ONLINESIM_API_KEY")
+	return fmt.Sprintf(`%s/%s.php?apikey=%s`, OnlinesimApiEndpoint, action, apiKey)
+}
+
+func client() *http.Client {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	return &http.Client{Transport: tr}
+}
+
+func getState(id int32, messageToCode int32, states *[]StateResponse) error {
+	return retry(MaxRetries, time.Second, func() error {
+		stateResponse := make([]StateResponse, 2, 2)
+
+		url := fmt.Sprintf(`%s&tzid=%d&message_to_code=%d&msg_list=1`,
+			apiUrl(`getState`), id, messageToCode)
+
+		log.Print(url)
+
+		resp, err := client().Get(url)
+
+		if err != nil {
+			log.Print(err)
+			return err
+		}
+
+		defer resp.Body.Close()
+
+		body, _ := ioutil.ReadAll(resp.Body)
+
+		log.Print(string(body[:]))
+
+		if err = json.Unmarshal(body, &stateResponse); err != nil {
+			log.Print(err)
+			return stop{error: err}
+		}
+
+		if stateResponse[0].Response == ResponseFail {
+			log.Print(stateResponse)
+			return stop{error: errors.New("Response Fail")}
+		}
+
+		*states = stateResponse
+
+		return nil
+	})
+}
+
+func numberReq(service string, idOp *int32) error {
+	return retry(MaxRetries, time.Second, func() error {
+		var numResponse NumberResponse
+
+		// Запрос на номер
+		url := fmt.Sprintf(`%s&service=%s&country=7`, apiUrl(`getNum`), service)
+		log.Print(url)
+		resp, err := client().Get(url)
+
+		if err != nil {
+			log.Print(err)
+			return err
+		}
+
+		defer resp.Body.Close()
+
+		body, _ := ioutil.ReadAll(resp.Body)
+
+		log.Print(string(body[:]))
+
+		if err = json.Unmarshal(body, &numResponse); err != nil {
+			log.Print(err)
+
+			return stop{error: err}
+		}
+
+		if numResponse.Response == ResponseFail {
+			log.Print(numResponse)
+			return stop{error: errors.New("Response Fail")}
+		}
+
+		*idOp = numResponse.Tzid
+
+		return nil
+	})
 }
